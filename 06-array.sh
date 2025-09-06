116eb4aaffe64de3d0a96cab9d91cc6b6039ae76
Eswar <eswar.chandana@apoorva.com>
2025-09-06 22:28:40 +0530

    hello world started

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+MOVIES = ("PUSHPA", "BAHUBALI", "SALAAAR")
+
+echo "First MovieL ${MOVIES[0]}"
+echo "First MovieL ${MOVIES[1]}"
+echo "First MovieL ${MOVIES[3]}"
+
+echo "All Movies: ${MOVIES[@]}" 
