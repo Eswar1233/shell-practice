a414a6cc5550b85246e67228609c87efe62ea6a4
Eswar <eswar.chandana@apoorva.com>
2025-09-06 18:09:06 +0530

    hello world started

---
+++ 02.conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Trump: Hey mush , how are you?"
+echo "Mush:: Helo Trumph , I am fine, How are you doing"
+echo "Trump: I am fine bro."
+echo "Mush:: Nothing , just going to mars man"
