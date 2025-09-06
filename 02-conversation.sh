41f2877a3ff8566418330d42eaa5c9ba0f35ea7a
Eswar <eswar.chandana@apoorva.com>
2025-09-06 18:18:55 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-PERSON1=$PERSON1
-PERSON2=$PERSON2
-
-echo "$PERSON1: Hey $PERSON2 , how are you?"
-echo "$PERSON2:: Helo $PERSON1 , I am fine, How are you doing"
-echo "$PERSON1: I am fine bro."
-echo "$PERSON2:: Nothing , just going to mars man"
+echo "Trump: Hey mush , how are you?"
+echo "Mush:: Helo Trumph , I am fine, How are you doing"
+echo "Trump: I am fine bro."
+echo "Mush:: Nothing , just going to mars man"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$PERSON1
+PERSON2=$PERSON2
+
+echo "$PERSON1: Hey $PERSON2 , how are you?"
+echo "$PERSON2:: Helo $PERSON1 , I am fine, How are you doing"
+echo "$PERSON1: I am fine bro."
+echo "$PERSON2:: Nothing , just going to mars man"
